505ab0b08c371da2c40d1c6736c5eb52a6eeb5e3
DarthHamza <hamzamakia@gmail.com>
2018-04-16 14:00:01 +0300

    static files setup

--- task_08/urls.py
+++ task_08/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from restaurants import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,6 @@ urlpatterns = [
     path('restaurants/update/<int:restaurant_id>/',views.restaurant_update ,name='restaurant-update'),
     path('restaurants/delete/<int:restaurant_id>/',views.restaurant_delete ,name='restaurant-delete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
